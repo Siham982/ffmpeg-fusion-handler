9f8d6191a0ffe2a6222e89687b9ff2bafb39d2fc
Siham982 <siham.zaak@gmail.com>
2025-08-24 18:02:44 +0200

    Update handler.py

--- handler.py
+++ handler.py
@@ -12,14 +12,11 @@ def handler(event):
     audio_url = event['input']['audio_url']
     command = event['input']['command']
 
-    # Télécharger les fichiers
     download_file(video_url, 'video.mp4')
     download_file(audio_url, 'audio.mp3')
 
-    # Exécuter la commande FFmpeg
     subprocess.run(command, shell=True)
 
-    # Retourne un message simple (le fichier est dans le container)
     return {
         "message": "Fusion terminée.",
         "output_file": "output.mp4"
